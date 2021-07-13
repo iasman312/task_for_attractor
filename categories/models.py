26989a18a74dde062172fd5b454ff90fca00f968
asman <iasman312>
2021-07-13 14:05:46 +0600

    Создал модель категории

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=120,
+        null=False,
+        blank=False,
+        verbose_name='Название')
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
