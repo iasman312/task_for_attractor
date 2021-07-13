7bfc669d882d64fb6f1e39daff1f1500b02fc8e6
asman <iasman312>
2021-07-13 14:16:30 +0600

    Создал модель статьи

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    category = models.ForeignKey(
+        'categories.Category',
+        on_delete=models.CASCADE,
+        related_name='articles',
+        verbose_name='Категория',
+        null=False,
+        blank=False
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='user_articles',
+        verbose_name='Пользователь',
+        null=True,
+        blank=True
+    )
+    title = models.CharField(
+        max_length=120,
+        null=False,
+        blank=False,
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        max_length=3000,
+        null=True,
+        blank=True,
+        verbose_name='Описание'
+    )
+    image = models.ImageField(
+        upload_to='images',
+        null=True,
+        blank=True,
+        verbose_name='Картинка'
+    )
