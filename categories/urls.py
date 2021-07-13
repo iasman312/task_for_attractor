db503b570ef39fa01e10a9531ac86f37ef38faaf
asman <iasman312>
2021-07-14 01:15:49 +0600

    Добавили путь представление для создания категория в urls

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
-from categories.views import IndexView, ArticleView
+from categories.views import IndexView, ArticleView, CreateCategoryView
+
+app_name = 'category'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='category-list'),
-    path('<int:pk>/', ArticleView.as_view(), name='category-view'),
+    path('', IndexView.as_view(), name='list'),
+    path('create/', CreateCategoryView.as_view(), name='create'),
+    path('<int:pk>/', ArticleView.as_view(), name='view'),
 ]
