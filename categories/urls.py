eaf8a3069bf85465845985bfa5b22f5aea7aeabe
asman <iasman312>
2021-07-14 01:44:44 +0600

    Добавил представление для удаления категория в урлы

--- categories/urls.py
+++ categories/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from categories.views import (
     IndexView,
     CategoryView,
     CategoryCreateView,
     CategoryUpdateView,
+    CategoryDeleteView,
 )
 
 app_name = 'category'
@@ -13,5 +14,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='list'),
     path('create/', CategoryCreateView.as_view(), name='create'),
     path('<int:pk>/', CategoryView.as_view(), name='view'),
-    path('<int:pk>/update/', CategoryUpdateView.as_view(), name='update')
+    path('<int:pk>/update/', CategoryUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', CategoryDeleteView.as_view(), name='delete')
 ]
