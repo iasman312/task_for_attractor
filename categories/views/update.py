81e22b42308b7a1e7ecf9984c8e858ece89dc39a
asman <iasman312>
2021-07-14 01:35:12 +0600

    Создал представление для редактирования категория

---
+++ categories/views/update.py
@@ -0,0 +1,15 @@
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from categories.forms import CategoryForm
+from categories.models import Category
+
+
+class CategoryUpdateView(UpdateView):
+    form_class = CategoryForm
+    model = Category
+    template_name = 'categories/update.html'
+    context_object_name = 'category'
+
+    def get_success_url(self):
+        return reverse('category:view', kwargs={'pk': self.kwargs.get('pk')})
