cc65ad73c5f8e1df51087028500cc0f891d5df17
asman <iasman312>
2021-07-14 01:36:37 +0600

    Добавил урл для редактирования категория

--- categories/urls.py
+++ categories/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 
-from categories.views import IndexView, ArticleView, CreateCategoryView
+from categories.views import (
+    IndexView,
+    CategoryView,
+    CategoryCreateView,
+    CategoryUpdateView,
+)
 
 app_name = 'category'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='list'),
-    path('create/', CreateCategoryView.as_view(), name='create'),
-    path('<int:pk>/', ArticleView.as_view(), name='view'),
+    path('create/', CategoryCreateView.as_view(), name='create'),
+    path('<int:pk>/', CategoryView.as_view(), name='view'),
+    path('<int:pk>/update/', CategoryUpdateView.as_view(), name='update')
 ]
