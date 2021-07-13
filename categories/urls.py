818a26852efc1f513f88f3d3be455b8fdf2a4849
asman <iasman312>
2021-07-14 00:56:28 +0600

    Добавил путь к представлению детального просмотра категорий

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from categories.views import IndexView
+from categories.views import IndexView, ArticleView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='category-list')
+    path('', IndexView.as_view(), name='category-list'),
+    path('<int:pk>/', ArticleView.as_view(), name='category-view'),
 ]
