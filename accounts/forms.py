b75cd8e7c14a242537945790dfaa981e44da4d51
asman <iasman312>
2021-07-14 13:58:59 +0600

    Создали форму для смены пароля

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms.widgets import PasswordInput
 
 
 class UserRegisterForm(UserCreationForm):
@@ -12,3 +13,36 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ('username',)
+
+
+class UserChangePasswordForm(forms.ModelForm):
+    old_password = forms.CharField(required=True, label='Старый пароль', widget=PasswordInput)
+    new_password = forms.CharField(required=True, label='Новый пароль', widget=PasswordInput)
+    password_confirm = forms.CharField(required=True, label='Подтверждение пароля', widget=PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('old_password', 'new_password', 'password_confirm')
+
+    def clean_password_confirm(self):
+        new_password = self.cleaned_data.get('new_password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+
+        if new_password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают')
+        return new_password
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Пароль введён не верно')
+
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data.get('new_password'))
+        if commit:
+            user.save()
+        return user
