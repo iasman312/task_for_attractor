2d6636de70750b3553c626e7b8503f46869518f8
asman <iasman312>
2021-07-14 12:58:04 +0600

    Создали форму для редактирования профиля

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class UserRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        fields = ('username',)
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
         fields = ('username',)
