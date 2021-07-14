b93e22f29e963368ade14267c5e3e623a7ca1149
asman <iasman312>
2021-07-14 16:12:28 +0600

    Создал сериализатор для отправки всех категорий

---
+++ api_v1/serializers/category_list.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from api_v1.serializers import ArticlesSerializer
+from categories.models import Category
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    articles = ArticlesSerializer(many=True)
+
+    class Meta:
+        fields = (
+            'title',
+            'articles'
+        )
+        model = Category
