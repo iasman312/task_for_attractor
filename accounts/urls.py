f1268fd926952423ea0a7470211703dce73338bd
asman <iasman312>
2021-07-14 12:39:32 +0600

    Добавил регистацию пользователя в urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from accounts.views import register_view
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register'),
 ]
