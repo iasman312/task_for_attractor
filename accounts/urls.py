c8fa9d08307f3017051c533b9e4ca0e1b30d46dd
asman <iasman312>
2021-07-14 12:48:15 +0600

    Добавил представление в urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import register_view
+from accounts.views import register_view, UserDetailView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
