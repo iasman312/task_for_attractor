e592ccff624fdad41e1a7d2203706183bdb1170f
asman <iasman312>
2021-07-14 12:09:42 +0600

    Создали представление для редактирования статьи

---
+++ articles/views/update.py
@@ -0,0 +1,15 @@
+from django.urls import reverse
+from django.views.generic import UpdateView
+
+from articles.forms import ArticleForm
+from articles.models import Article
+
+
+class ArticleUpdateView(UpdateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'articles/update.html'
+    context_object_name = 'article'
+
+    def get_success_url(self):
+        return reverse('category:view', kwargs={'pk': self.kwargs.get('pk')})
