c30c7b92fec5728b14b18f9d1b48f4adb8357d0c
asman <iasman312>
2021-07-14 12:20:57 +0600

    Добавили путь к представлению, которая отвечает за удаление статьи

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from articles.views import ArticleCreateView, ArticleUpdateView
+from articles.views import ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'article'
 
 urlpatterns = [
     path('articles/create/', ArticleCreateView.as_view(), name='create'),
     path('articles/update/', ArticleUpdateView.as_view(), name='update'),
+    path('articles/delete/', ArticleDeleteView.as_view(), name='delete'),
 ]
