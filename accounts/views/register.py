49e6d6d8a23151d2dda8749698a4190a7834e70c
asman <iasman312>
2021-07-14 12:38:59 +0600

    Создал представление для регистации пользователя

---
+++ accounts/views/register.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from accounts.forms import UserRegisterForm
+
+
+def register_view(request, *args, **kwargs):
+    context = {}
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('category:list')
+    context['form'] = form
+    return render(request, 'registration/register.html', context=context)
