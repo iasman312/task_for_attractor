4b9bbf09eb8cd108c67bd8c38f3f508c2f9deeac
asman <iasman312>
2021-07-14 13:59:32 +0600

    Добавили смену пароля в init

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -1,3 +1,4 @@
 from accounts.views.register import register_view
 from accounts.views.user_detail import UserDetailView
 from accounts.views.user_update import UserUpdateView
+from accounts.views.password_update import UserChangePasswordView
